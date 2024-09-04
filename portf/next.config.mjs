f46a65f06b8b5e526d225912d588110c4100c206
Anjana2k02 <101281237+Anjana2k02@users.noreply.github.com>
2024-09-04 22:40:30 +0530

    ss

--- portf/next.config.mjs
+++ portf/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Enables static export
+    basePath: '/portfolio12', // Add this if your repository is named 'portfolio12'
   };
   
   export default nextConfig;
